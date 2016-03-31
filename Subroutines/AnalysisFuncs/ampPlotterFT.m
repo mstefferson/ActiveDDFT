050e8759fb94e7a42aeee0b822e9b78239b627a1
mstefferson <michael.stefferson@colorado.edu>
2016-03-31 12:06:41 -0600

    Ampploter no longer prints parameters because that was causing issues with textbp. I don't think the text was fitting.

--- Subroutines/AnalysisFuncs/ampPlotterFT.m
+++ Subroutines/AnalysisFuncs/ampPlotterFT.m
@@ -6,12 +6,12 @@ km0 = Nm / 2 + 1;
 
 % keyboard
 
-ParamStrNx = sprintf('Nx = %d', Nx);
-ParamStrNy = sprintf('Ny = %d', Ny);
-ParamStrNm = sprintf('Nm = %d', Nm);
-ParamStrBc = sprintf('bc = %.2f', bc);
-ParamStrVd = sprintf('vD = %.2f', vD);
-ParamStrTr = sprintf('trial = %d', trial);
+ParamStrNx = sprintf('Nx:%d', Nx);
+ParamStrNy = sprintf('Ny:%d', Ny);
+ParamStrNm = sprintf('Nm:%d', Nm);
+ParamStrBc = sprintf('bc:%.2f', bc);
+ParamStrVd = sprintf('vD:%.2f', vD);
+ParamStrTr = sprintf('t:%d', trial);
 
 ParamStrCell = {ParamStrNx;  ParamStrNy; ParamStrNm;' ';...
                          ParamStrTr;ParamStrBc; ParamStrVd;' ' };
@@ -19,11 +19,10 @@ ParamStrCell = {ParamStrNx;  ParamStrNy; ParamStrNm;' ';...
 
 figure()
 
-% keyboard
 for i = 1:8
     subplot(2,4,i)
     [Ax, ~, ~] = plotyy( TimeRec, real( FTmat2plot(i,:) ) ,TimeRec, imag( FTmat2plot(i,:) ) );
-    titstr = sprintf('Mode (%d, %d, %d)', ...
+    titstr = sprintf('(%d, %d, %d)', ...
         FTind2plot(i,1) - kx0, FTind2plot(i,2) - ky0, FTind2plot(i,3) - km0 );
     title(titstr)
     xlabel('Time')
@@ -31,7 +30,9 @@ for i = 1:8
     if(i == 1 || i == 5 ); ylabel(Ax(1),' real Amp'); end
     if(i == 4 || i == 8 ); ylabel(Ax(2),' imag Amp'); end
     
-    textbp(ParamStrCell{i})
+%     keyboard
+%     textbp(ParamStrCell{i})
+%     textbp('helloasdfadfa')
 end
 
 if SaveMe
