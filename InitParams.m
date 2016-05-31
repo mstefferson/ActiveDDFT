8ae53fb99291a03bf8c435a3d46b82bc7549e8b5
Michael Stefferson <mstefferson@gmail.com>
2016-05-31 10:04:14 -0600

    InitParams and RunHardRod are sucessfully looping over parameters. Ready to integrate the Main body into the new step up

--- InitParams.m
+++ InitParams.m
@@ -1,36 +1,43 @@
 % Anisotropic diffusion
 % Input creater for HR2DrotMainDr
 
-cd ~/DDFT/HardRodML
+%cd ~/DDFT/HardRodML
 CurrentDir = pwd;
 addpath( genpath( CurrentDir ) );
 
-% Main Flags
-Flags.SaveMe       = 1; % Saving
-Flags.AnisoDiff    = 1; % Aniso = 1. Iso = 0
-Flags.Interactions = 1; % Int = 1. No Int = 0
-Flags.MakeMovies   = 1; % No movies if save is zero
-Flags.MakeOP       = 1; % No OPs if save is zero
-% Stepping---0: AB1 1: AB2 2: HAB1 3: HAB2 4: BHAB1 5: BHAB2 6: phiV- Aniso EE-Iso
-Flags.StepMeth     = 1;
+% Main FlagMaster
+FlagMaster.SaveMe       = 1; % Saving
+FlagMaster.AnisoDiff    = 1; % Aniso = 1. Iso = 0
+FlagMaster.Interactions = 1; % Int = 1. No Int = 0
+FlagMaster.MakeMovies   = 1; % No movies if save is zero
+FlagMaster.MakeOP       = 1; % No OPs if save is zero
+FlagMaster.AllNsSame    = 0; % Sets all gridptns to be the same
+FlagMaster.SquareBox    = 0; % Forces box to be square
+FlagMaster.StepMeth     = [1]; % Stepping (integrating) method (vec)
+% 0: AB1 1: AB2 2: HAB1 3: HAB2 4: BHAB1 5: BHAB2 6: phiV- Aniso EE-Iso
 
 %%%%%% Parameters %%%%%%%%%%%%%%%%%
 % Can be vector if (vec) is in comment
-ParamMaster.trial   = 200;  % Trial Indicator
-ParamMaster.Nx      = 64; % Gridpoints in x dir (vec)
-ParamMaster.Ny      = 64; % Gridpoints in y dir (vec) 
-ParamMaster.Nm      = 64; % Gridpoints in angle (vec)
+ParamMaster.trial   = 1;  % Trial Indicator
+ParamMaster.runID   = 1;    % Run indicator *_trial.runID
+ParamMaster.Nx      = [64]; % Gridpoints in x dir (vec)
+ParamMaster.Ny      = [64]; % Gridpoints in y dir (vec) 
+ParamMaster.Nm      = [64]; % Gridpoints in angle (vec)
 ParamMaster.bc      = 1.45; % Scaled concentration (vec)
 ParamMaster.L_rod   = 1;  % Length of the rods
-ParamMaster.Lx      = 10*L_rod;  % Box length (vec)
-ParamMaster.Ly      = 10*L_rod;  % Box length (vec)
+ParamMaster.Lx      = [10]*ParamMaster.L_rod;  % Box length (vec)
+ParamMaster.Ly      = [10]*ParamMaster.L_rod;  % Box length (vec)
 ParamMaster.vD      = 30.0; % Driving velocity (vec)
 ParamMaster.Tmp      = 1;            % Temperature
 % mobility
 Mob = 1;
-if Flags.Aniso; ParamMaster.Mob_par  = 2*Mob; else; ParamMaster.Mob_par = Mob; end;
+if FlagMaster.AnisoDiff; 
+  ParamMaster.Mob_par  = 2*Mob; 
+else
+  ParamMaster.Mob_par = Mob; 
+end
 ParamMaster.Mob_perp   = Mob;
-ParamMaster.Mob_rot   = 6 * Mob / L_rod^2;
+ParamMaster.Mob_rot   = 6 * Mob / ParamMaster.L_rod^2;
 
 %%%%%%%%%%%%%%% Time %%%%%%%%%%%%%%%%%%%%%%%%%%
 TimeObj.delta_t     = 1e-3; % time step
@@ -39,20 +46,26 @@ TimeObj.t_tot       = 10.0; % total time
 TimeObj.ss_epsilon  = 1e-8;  % steady state condition
 
 %%%%%%%%% Initial Condition %%%%%%%%%%%%%%%%%%%%%
-RhoInit.IntCond     = 1;
-RhoInit.NumModesX   = 8;
-RhoInit.NumModesY   = 8;
-RhoInit.NumModesM   = 8;
+RhoInitMaster.IntCond     = [1]; % IC indicator (vec)
+RhoInitMaster.NumModesX   = 8; % Perturb # modes x
+RhoInitMaster.NumModesY   = 8; % Perturb # modes y
+RhoInitMaster.NumModesM   = 8; % Perturb # modes m
 % Don't perturb more more than you are allowed to
-if( NumModesX >= Nx / 2 ); NumModesX = floor(Nx / 2) - 2; end;
-if( NumModesY >= Ny / 2 ); NumModesY = floor(Ny / 2) - 2; end;
-if( NumModesM >= Nm / 2 ); NumModesM = floor(Nm / 2) - 2; end;
+if( RhoInitMaster.NumModesX >= ParamMaster.Nx / 2 ); 
+  RhoInitMaster.NumModesX = floor(ParamMaster.Nx / 2) - 2; 
+end;
+if( RhoInitMaster.NumModesY >= ParamMaster.Ny / 2 ); 
+  RhoInitMaster.NumModesY = floor(ParamMaster.Ny / 2) - 2;
+end;
+if( RhoInitMaster.NumModesM >= ParamMaster.Nm / 2 );
+  RhoInitMaster.NumModesM = floor(ParamMaster.Nm / 2) - 2; 
+end;
 
 % Weight of the spatial sinusoidal perturbation. %
 % Perturbations added to rho(i,j,k) = 1. Must be small
-RhoInit.WeightPos   = 1e-3;
-RhoInit.WeightAng   = 1e-3;
-RhoInit.RandomAmp   = 1;       % Random perturbation coeffs
+RhoInitMaster.WeightPos   = 1e-3;
+RhoInitMaster.WeightAng   = 1e-3;
+RhoInitMaster.RandomAmp   = 1;       % Random perturbation coeffs
 
 % Key
 % The number of k-modes above and below k = 0 added as a perturbation
@@ -67,20 +80,36 @@ RhoInit.RandomAmp   = 1;       % Random perturbation coeffs
 
 % Calculated stuff- fix times, etc.
 
+if FlagMaster.SquareBox == 1; 
+  ParamMaster.L_box = unique( [ParamMaster.Lx ParamMaster.Ly] );
+  ParamMaster.Lx = ParamMaster.L_box; 
+  ParamMaster.Ly = ParamMaster.L_box; 
+end
+
+if FlagMaster.AllNsSame == 1; 
+  Nvec = unique( [ParamMaster.Nx ParamMaster.Ny ParamMaster.Nm] );
+  ParamMaster.Nx = Nvec;
+  ParamMaster.Ny = Nvec; 
+  ParamMaster.Nm = Nvec;
+end
+
+
 % Concentration and rod stuff
 b       = ParamMaster.L_rod^2/pi;               % Average excluded volume per particle
-ParamMaster.c       = bc / b;                   % Concentration
+c       = ParamMaster.bc / b;                   % Concentration
+ParamMaster.Mob_pos = Mob;
+
 %%%%%%%%%%%%%% DO THIS IN RUN %%%%%%%%%%%%%
-ParamMaster.Norm    = c * Lx * Ly;              % number of particles
+
 
 % Fix the time
 [TimeObj.t_tot,TimeObj.N_time,TimeObj.t_rec,TimeObj.N_rec,TimeObj.N_count]= ...
-        TimeStepRecMaker(delta_t,t_tot,t_record);
+        TimeStepRecMaker(TimeObj.delta_t,TimeObj.t_tot,TimeObj.t_record);
 
 % Turn movies off is Save is off
-if Flags.SaveMe == 0; Flags.MakeOP = 0; Flags.MakeMovies = 0;;end
-if Flags.MakeMovies == 1; Flag.MakeOP = 1; end % if make movie, make OP first
-if ParamMaster.vD  == 0; Flags.Drive = 0; else Flags.Drive = 1;end
+if FlagMaster.SaveMe == 0; FlagMaster.MakeOP = 0; FlagMaster.MakeMovies = 0;;end
+if FlagMaster.MakeMovies == 1; Flag.MakeOP = 1; end % if make movie, make OP first
+if ParamMaster.vD  == 0; FlagMaster.Drive = 0; else FlagMaster.Drive = 1;end
 
 % Save the Params
-save('Params.mat','ParamMaster','TimeObj','Flags','RhoInit')
+save('Params.mat','ParamMaster','TimeObj','FlagMaster','RhoInitMaster')
--- RunHardRod.m
+++ RunHardRod.m
@@ -4,112 +4,235 @@ DateTimeStart =  datestr(now);
 fprintf('Starting RunHardRod: %s\n', DateTimeStart);
 
 % Print what you are doing
-if FlagObj.AnisoDiff  == 1;
-fprintf('Anisotropic Hard Rod \n')
+if Flags.AnisoDiff  == 1;
+  fprintf('Anisotropic Hard Rod \n')
 else
-fprintf('Isotropic Hard Rod \n')
+  fprintf('Isotropic Hard Rod \n')
 end
 
 % Grab initial parameters
 if exist('Params.mat','file') == 0;
-    if exist('InitParams.m','file') == 0;
-      cpmatparams
-    end;
-    InitParams
+  if exist('InitParams.m','file') == 0;
+    cpmatparams
+  end;
+  InitParams
 end
 load Params.mat;
 % Copy the master parameter list to ParamObj
 ParamObj = ParamMaster;
-
-NumNx = length(ParamObj.Nx);
-NumNy = length(ParamObj.Ny);
-NumNm = length(ParamObj.Nm);
-NumLx = length(ParamObj.Lx);
-NumLy = length(ParamObj.Ly);
-Numbc = length(ParamObj.bc);
-NumvD = length(ParamObj.vD);
-NumParams = NumNx * NumNy * NumNm * NumLx * NumLy * Numbc * NumvD;
+RhoInit  = RhoInitMaster;
+Flags    = FlagMaster;
 
 
-if  strcmp( IntConcStr,'PlaneWaveEq' ) || strcmp( IntConcStr,'SepPWeq' )
-    if 1.499 < bc && bc < 1.501
-        bc = 1.502;
-    end
-end
+%Find number parameters
+numbc = length(ParamObj.bc);
+numvD = length(ParamObj.vD);
+numIC = length(RhoInit.IntCond);
+numSM = length(Flags.StepMeth);
 
-if strcmp( IntConcStr,'Loaded' )
-    IntDenName = sprintf('DenBlow2');
+% Handls all grid points equal and square box different
+if Flags.AllNsSame == 1
+  Nvec = unique( [ParamObj.Nx ParamObj.Ny ParamObj.Nm] );
+  numN = length(Nvec);
+  numNx = numN;
+  numNy = 1;
+  numNm = 1;
 else
-    IntDenName = sprintf('Irrelevant');
+  numNx = length(ParamObj.Nx);
+  numNy = length(ParamObj.Ny);
+  numNm = length(ParamObj.Nm);
+  numN = numNx * numNy * numNm;
 end
 
-% Make the output directory string and input file
- if SaveMe
-     if ~exist('Outputs', 'dir'); mkdir('Outputs'); end;
-     DiaryStr = sprintf('DiarySingRunt%d.log',trial);
-     diary(DiaryStr);
-     runfile  = 'AnisRunLog.log';
-     rlId = fopen(runfile,'a+'); 
-%      fprintf(rlId, 'Anisotropic Hard Rod Hard Rod \n');
-    
- end
+if Flags.SquareBox == 1
+  Lvec = unique( [ParamMaster.Lx ParamMaster.Ly] );
+  numL = length(Lvec);
+  numLx = numL;
+  numLy = 1;
+else
+  numLx = length(ParamObj.Lx);
+  numLy = length(ParamObj.Ly);
+  numL = numLx * numLy;
+end
 
-FileDir = ...
-    sprintf('HrAnD%i%i%i_bc%.2f_Int%i_v%.1f_IC%dsm%dt%d',...
-    Nx,Ny,Nm,bc,Interactions,vD,IntCond,StepMeth,trial);
-FileInpt = ...
-    sprintf('InptAnD_N%i%i%i_bc%.2f_Int%i_v%.1f_IC%dsm%dt%d.txt', ...
-    Nx,Ny,Nm,bc,Interactions,vD,IntCond,StepMeth,trial);
+% number of parameters
+numNxNy = numNx*numNy;
+numNxNyNm = numNxNy * numNm;
+numNxNyNmLx = numNxNyNm * numLx;
+numNxNyNmLxLy = numNxNyNmLx * numLy;
+numNxNyNmLxLyvD = numNxNyNmLxLy * numvD;
+numParams = numNxNyNmLxLyvD * numbc;
+numParamsIC = numParams * numIC;
+numRuns = numParamsIC * numSM;
 
-Where2SavePath    = sprintf('%s/%s/%s',pwd,'Outputs',FileDir);
-
-% Figure out Total Runs
-    tic
-    %     keyboard
-    [DenFinal, DenFTFinal, GridObj, ParamMaster,TimeObj,...
-        DidIBreak,SteadyState,MaxReldRho] = ...
-        HR2DrotMain(FileInpt);
-    toc
-    disp('Params');disp(ParamMaster);disp('Time');disp(TimeObj);      
-    fprintf('Break = %d Steady = %d Max dRho/Rho = %.2e\n',...
-        DidIBreak,SteadyState,MaxReldRho)
-    DateTimeEnd =  datestr(now);
-    fprintf('End: %s\n\n', DateTimeEnd);
-    
-    if SaveMe
-        diary off; 
-        if ~exist(Where2SavePath, 'dir'); mkdir(Where2SavePath); end
-        movefile('*.mat', Where2SavePath)
-        movefile('*.txt', Where2SavePath)
-        movefile('Diary*', Where2SavePath)
-        if MakeMovies
-            movefile('*.avi', Where2SavePath)
+% Create Paramater matrix
+% paramMat columns: (Nx, Ny, Nm, Lx, Ly, vD, bc, IC, SM, runID)
+paramMat = zeros( numRuns, 10);
+for i = 1:numNx
+  for j = 1:numNy
+    for k = 1:numNm
+      for l = 1:numLx
+        for m = 1:numLy
+          for n = 1:numvD
+            for o = 1:numbc
+              for p = 1:numIC
+                for q = 1:numSM
+                  rowInd =  1 + (i-1) + (j-1) * numNx + ( k-1) * numNxNy + ...
+                    (l-1) * numNxNyNm + (m-1) * numNxNyNmLx + ...
+                    (n-1) * numNxNyNmLxLy + (o-1) * numNxNyNmLxLyvD + ...
+                    (p-1) * numParams + (q-1) * numParamsIC;
+                  
+                  % Special cases
+                  if Flags.AllNsSame
+                    paramMat(rowInd,1:3) = Nvec(i);
+                  else
+                    paramMat(rowInd,1) = ParamObj.Nx(i);
+                    paramMat(rowInd,2) = ParamObj.Ny(j);
+                    paramMat(rowInd,3) = ParamObj.Nm(k);
+                  end
+                  
+                  if Flags.SquareBox
+                    paramMat(rowInd,4:5) = Lvec(l);
+                  else
+                    paramMat(rowInd,4) = ParamObj.Lx(l);
+                    paramMat(rowInd,5) = ParamObj.Ly(m);
+                  end
+                  % Everything else
+                  paramMat(rowInd,6) = ParamObj.vD(n);
+                  paramMat(rowInd,7) = ParamObj.bc(o);
+                  paramMat(rowInd,8) = RhoInit.IntCond(p);
+                  paramMat(rowInd,9) = Flags.StepMeth(q);
+                  paramMat(rowInd,10) = ParamObj.runID - 1 + rowInd;
+                end
+              end
+            end
+          end
         end
-        if MakeOP
-            movefile('*.fig', Where2SavePath)
-            movefile('*.jpg', Where2SavePath)
-        end       
-        
-        rlId = fopen(runfile,'a+');
-        fprintf(rlId,'%s\n', FileInpt(1:end-4));
-        fprintf(rlId,'( dt t_rec t_tot ss_eps ): ( %.2e %.2e %.2e %.2e )\n',...
-            Timetmp);
-        fprintf(rlId,'( Nx, Ny, Nm ): ( %d, %d, %d )\n', Nx, Ny, Nm);
-        fprintf(rlId,'( Lx, Ly, Lrod ) = ( %.1f, %.1f, %.1f )\n',...
-         Lx, Ly, L_rod);
-        fprintf(rlId,...
-            '( bc, vD, Mob_par, Mob_perp, Mob_rot ) = ( %.2f, %.2f, %.1f, %.1f, %.1f )\n',...
-         bc, vD, Mob_par, Mob_perp, Mob_rot);
-        fprintf(rlId,'( MdX, MdY, MdM, rand) = ( %d, %d, %d, %d )\n',...
-         NumModesX, NumModesY, NumModesM, RandomAmp);
-        fprintf(rlId,'Break = %d Steady = %d Max dRho/Rho = %.2e\n',...
-        DidIBreak,SteadyState,MaxReldRho);
-        fprintf(rlId, 'Start: %s\n', DateTimeStart);
-        fprintf(rlId,'End: %s\n\n', DateTimeEnd);
-        fclose('all');
-    else
-        remove('*.txt')
+      end
     end
-
+  end
 end
+
+% For some reason, param_mat gets "sliced". Create vectors to get arround
+paramNx  = paramMat(:,1);
+paramNy  = paramMat(:,2);
+paramNm  = paramMat(:,3);
+paramLx  = paramMat(:,4);
+paramLy  = paramMat(:,5);
+paramvD  = paramMat(:,6);
+parambc  = paramMat(:,7);
+paramIC  = paramMat(:,8);
+paramSM  = paramMat(:,9);
+paramrun = paramMat(:,10);
+
+% Loops over all run
+
+for ii = 1:numRuns
+  % Assign parameters
+  ParamObj.Nx = paramNx(ii);
+  ParamObj.Ny = paramNy(ii);
+  ParamObj.Nm = paramNm(ii);
+  ParamObj.Lx = paramLx(ii);
+  ParamObj.Ly = paramLy(ii);
+  ParamObj.vD = paramvD(ii);
+  ParamObj.bc = parambc(ii);
+  RhoInit.IntCond = paramIC(ii);
+  Flags.StepMeth = paramSM(ii);
+  ParamObj.runID = paramrun(ii);
+  ParamObj.Norm  = ParamObj.bc / ( ParamObj.L_rod ^ 2 / pi ) *... % number of particles
+    ParamObj.Lx * ParamObj.Ly;
+  if Flags.SquareBox
+    ParamObj.L_box = ParamObj.Lx;
+  else
+    ParamObj.L_box = [ParamObj.Lx ParamObj.Ly];
+  end
+  
+  % Name the file
+  filename = [ 'Hr_Ani' num2str( Flags.AnisoDiff ) ...
+    '_N' num2str( ParamObj.Nx ) num2str( ParamObj.Ny ) num2str( ParamObj.Nm )  ...
+    '_Lx' num2str( ParamObj.Lx ) 'Ly' num2str( ParamObj.Ly )...
+    '_vD' num2str( ParamObj.vD ) '_bc' num2str( ParamObj.bc ) ...
+    '_IC' num2str( RhoInit.IntCond ) '_SM' num2str( Flags.StepMeth ) ...
+    '_t' num2str( ParamObj.trial ) '.' num2str( ParamObj.runID ) '.mat' ];
+  
+  disp(filename);
+  disp(ParamObj);
+  
+end
+%%
+%if  strcmp( IntConcStr,'PlaneWaveEq' ) || strcmp( IntConcStr,'SepPWeq' )
+%if 1.499 < bc && bc < 1.501
+%bc = 1.502;
+%end
+%end
+
+
+%% Make the output directory string and input file
+%if SaveMe
+%if ~exist('Outputs', 'dir'); mkdir('Outputs'); end;
+%DiaryStr = sprintf('DiarySingRunt%d.log',trial);
+%diary(DiaryStr);
+%runfile  = 'AnisRunLog.log';
+%rlId = fopen(runfile,'a+');
+%%      fprintf(rlId, 'Anisotropic Hard Rod Hard Rod \n');
+%end
+
+%FileDir = ...
+%sprintf('HrAnD%i%i%i_bc%.2f_Int%i_v%.1f_IC%dsm%dt%d',...
+%Nx,Ny,Nm,bc,Interactions,vD,IntCond,StepMeth,trial);
+%FileInpt = ...
+%sprintf('InptAnD_N%i%i%i_bc%.2f_Int%i_v%.1f_IC%dsm%dt%d.txt', ...
+%Nx,Ny,Nm,bc,Interactions,vD,IntCond,StepMeth,trial);
+
+%Where2SavePath    = sprintf('%s/%s/%s',pwd,'Outputs',FileDir);
+
+%% Figure out Total Runs
+%tic
+%%     keyboard
+%[DenFinal, DenFTFinal, GridObj, ParamMaster,TimeObj,...
+%DidIBreak,SteadyState,MaxReldRho] = ...
+%HR2DrotMain(FileInpt);
+%toc
+%disp('Params');disp(ParamMaster);disp('Time');disp(TimeObj);
+%fprintf('Break = %d Steady = %d Max dRho/Rho = %.2e\n',...
+%DidIBreak,SteadyState,MaxReldRho)
+%DateTimeEnd =  datestr(now);
+%fprintf('End: %s\n\n', DateTimeEnd);
+
+%if SaveMe
+%diary off;
+%if ~exist(Where2SavePath, 'dir'); mkdir(Where2SavePath); end
+%movefile('*.mat', Where2SavePath)
+%movefile('*.txt', Where2SavePath)
+%movefile('Diary*', Where2SavePath)
+%if MakeMovies
+%movefile('*.avi', Where2SavePath)
+%end
+%if MakeOP
+%movefile('*.fig', Where2SavePath)
+%movefile('*.jpg', Where2SavePath)
+%end
+
+%rlId = fopen(runfile,'a+');
+%fprintf(rlId,'%s\n', FileInpt(1:end-4));
+%fprintf(rlId,'( dt t_rec t_tot ss_eps ): ( %.2e %.2e %.2e %.2e )\n',...
+%Timetmp);
+%fprintf(rlId,'( Nx, Ny, Nm ): ( %d, %d, %d )\n', Nx, Ny, Nm);
+%fprintf(rlId,'( Lx, Ly, Lrod ) = ( %.1f, %.1f, %.1f )\n',...
+%Lx, Ly, L_rod);
+%fprintf(rlId,...
+%'( bc, vD, Mob_par, Mob_perp, Mob_rot ) = ( %.2f, %.2f, %.1f, %.1f, %.1f )\n',...
+%bc, vD, Mob_par, Mob_perp, Mob_rot);
+%fprintf(rlId,'( MdX, MdY, MdM, rand) = ( %d, %d, %d, %d )\n',...
+%NumModesX, NumModesY, NumModesM, RandomAmp);
+%fprintf(rlId,'Break = %d Steady = %d Max dRho/Rho = %.2e\n',...
+%DidIBreak,SteadyState,MaxReldRho);
+%fprintf(rlId, 'Start: %s\n', DateTimeStart);
+%fprintf(rlId,'End: %s\n\n', DateTimeEnd);
+%fclose('all');
+%else
+%remove('*.txt')
+%end
+
+%end
 
