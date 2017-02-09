64453194e28f9caf139e93c4b83bc7c3b38f10f3
Michael Stefferson <mstefferson@gmail.com>
2017-02-09 11:28:44 -0700

    Added any function to BrokenDensityTracker---now correctly find negative elements

--- src/TrackerFuncs/BrokenSteadyDenTracker.m
+++ src/TrackerFuncs/BrokenSteadyDenTracker.m
@@ -5,26 +5,21 @@
 
 function [SteadyState,ShitIsFucked,MaxReldRho] = ...
   BrokenSteadyDenTracker(rho, rhoPrev, rho_FT, constConc,timeObj,systemObj)
-
+% Initialize
 SteadyState = 0;
 ShitIsFucked = 0;
-
+% Max density change
 WeightDensityChange =  abs( 1 - rhoPrev ./ rho  );
-
 MaxReldRho = max( max( max(WeightDensityChange) ) );
-
 if MaxReldRho < timeObj.ss_epsilon_dt
   SteadyState = 1;
 end
-
 %See if something broke
-
 %Negative Density check
-if rho < 0
+if any( rho < 0 )
   fprintf('Forgive me, your grace. Density has become negative\n');
   ShitIsFucked  = 1;
 end
-
 %Not conserving density check.
 if systemObj.n3 ~= 1
   constConcNow = rho_FT(systemObj.n1/2+1, systemObj.n2/2+1,systemObj.n3/2+1 );
@@ -33,19 +28,15 @@ if systemObj.n3 ~= 1
     ShitIsFucked  = 1;
   end
 end
-
 % Nan or infinity
-if isinf(rho)
+if any( isinf(rho) )
   fprintf('Forgive me, your grace. Density has gone infinite. ');
   fprintf('Does that make sense? No. No it does not\n');
   ShitIsFucked  = 1;
 end
 
-if isnan(rho)
+if any( isnan(rho) )
   fprintf('Forgive me, your grace. Density elements are no longer numbers. ');
   fprintf('Does that make sense? No. No it does not\n');
   ShitIsFucked  = 1;
-end
-
-
 end
