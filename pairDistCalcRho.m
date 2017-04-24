502ce2c0b49be2abc210ebb31256b8286e8736c6
Michael Stefferson <mstefferson@gmail.com>
2017-04-24 17:21:33 -0600

    Began new edits on pairDistCalcRho that should match HSK

--- pairDistCalcRho.m
+++ pairDistCalcRho.m
@@ -1,7 +1,7 @@
 % [pDist] = pairDistCalcRho(rho,l1,l2,lRod)
-% 
+%
 % Calculates the density dependent pair correlation function
-% g(r,r') = rho^(2) ./ rho^(1)rho^(1)  
+% g(r,r') = rho^(2) ./ rho^(1)rho^(1)
 %
 function [pDistAve] = pairDistCalcRho(rho,l1,l2,lRod,plotflag)
 % add paths just in case
@@ -12,8 +12,8 @@ addpath( genpath( [currentDir '/src'] ) );
 dx1 = l1 ./ n1;
 dx2 = l2 ./ n2;
 dphi = 2 * pi / n3;
-x1 = dx1 .* (-n1/2 + 1:n1/2 ); 
-x2 = dx2 .* (-n2/2 + 1:n2/2 ); 
+x1 = dx1 .* (-n1/2 + 1:n1/2 );
+x2 = dx2 .* (-n2/2 + 1:n2/2 );
 phi = 0 : dphi : 2*pi - dphi;
 % get MayerFunction
 [mayer] = mayerFncHr(n1, n2, n3, l1, l2, lRod) ;
@@ -29,7 +29,40 @@ totalInds2 = 2 .* maxDelta2 + 1;
 indsDelta3 = 1:n3;
 % initialize
 pDist = ones( n1, n2,n1, n2);
-vec2Int = zeros( totalInds1, totalInds2, n3 );
+% pDistNoNorm = zeros( n1, n2,n1, n2);
+% vec2Int = zeros( totalInds1, totalInds2, n3 );
+% for ii = 1:n1
+%   % inds for loop over delta x
+%   inds1 = mod( (ii-1) + indsDelta1 - 1, n1 ) + 1;
+%   for jj = 1:n2
+%     % inds for loop over delta y
+%     inds2 = mod( (jj-1) + indsDelta2 - 1, n2 ) + 1;
+%     for mm = 1:n3
+%       inds3 = mod( (mm-1) + indsDelta3 - 1, n3 ) + 1;
+%       mat2IntTemp = rho(ii, jj, mm) .* rho( inds1, inds2, inds3)...
+%         .* mayer( indsDelta1, indsDelta2, indsDelta3 );
+%       vec2Int(:,:,mm) = trapz_periodic( phi, mat2IntTemp, 3 );
+%     end
+%     num = trapz_periodic( phi, vec2Int, 3 );
+%     den = ( c(ii,jj) .* c(inds1, inds2) );
+%     newterm = num ./ den;
+% %     newtermNoNorm = den + num;
+%     reshapeNewterm = reshape( newterm, [1, 1, totalInds1, totalInds2] );
+% %     reshapeNewtermNoNorm = reshape( newtermNoNorm, [1, 1, totalInds1, totalInds2] );
+%     pDist( ii, jj, inds1, inds2 ) = pDist( ii, jj, inds1, inds2 ) + reshapeNewterm;
+% %     pDistNoNorm( ii, jj, inds1, inds2 ) = reshapeNewtermNoNorm;
+%   end
+% end
+
+%% new attempt
+expV =  mayer + 1;
+pDistNoNorm = zeros( n1, n2);
+% vec2Int = zeros( totalInds1, totalInds2, n3 );
+vec2Int = zeros( n1, n2, n3 );
+% indsDelta1 = 1:n1;
+indsDelta1 = 1:n1;
+% indsDelta2 = 1:n2;
+indsDelta2 = 1:n2;
 for ii = 1:n1
   % inds for loop over delta x
   inds1 = mod( (ii-1) + indsDelta1 - 1, n1 ) + 1;
@@ -38,19 +71,18 @@ for ii = 1:n1
     inds2 = mod( (jj-1) + indsDelta2 - 1, n2 ) + 1;
     for mm = 1:n3
       inds3 = mod( (mm-1) + indsDelta3 - 1, n3 ) + 1;
-      mat2IntTemp = rho(ii, jj, mm) .* rho( inds1, inds2, inds3)...
-        .* mayer( indsDelta1, indsDelta2, indsDelta3 );
+      mat2IntTemp = rho(indsDelta1, indsDelta2, indsDelta3) .* rho( inds1, inds2, inds3)...
+        .* expV( indsDelta1, indsDelta2, indsDelta3 );
       vec2Int(:,:,mm) = trapz_periodic( phi, mat2IntTemp, 3 );
     end
     num = trapz_periodic( phi, vec2Int, 3 );
-    den = ( c(ii,jj) .* c(inds1, inds2) );
-    newterm = num ./ den;
-    reshapeNewterm = reshape( newterm, [1, 1, totalInds1, totalInds2] );
-    pDist( ii, jj, inds1, inds2 ) = pDist( ii, jj, inds1, inds2 ) + reshapeNewterm;
+    pDistNoNorm( ii, jj ) = trapz_periodic( x2, trapz_periodic( x1, num, 1), 2 );
   end
 end
-% Calculate average
-pDistAve = ones( n1, n2);
+
+%% Calculate average
+pDistAve= ones( n1, n2);
+% pDistAveNoNorm = ones( n1, n2);
 combInd = combvec( 1:n1, 1:n2 );
 allInds1 = combInd(1,:).';
 allInds2 = combInd(2,:).';
@@ -61,18 +93,30 @@ for ii = indsDelta1
     jj2 = mod( allInds2 + (jj-2), n2 ) + 1 ;
     inds = sub2ind( matSize, allInds1', allInds2', ii2',  jj2' );
     tempMean =  mean( pDist( inds ) );
+%     tempMeanNoNorm =  mean( pDistNoNorm( inds ) );
     pDistAve(ii,jj) =  tempMean;
+%     pDistAveNoNorm(ii,jj) = tempMeanNoNorm;
   end
 end
 % center
 center1 = round( n1 / 2 ) + 1 ;
 center2 = round( n2 / 2 ) + 1 ;
 pDistAve = circshift( circshift( pDistAve, center1-1, 1 ), center2-1, 2 );
+pDistNoNorm = circshift( circshift( pDistNoNorm, center1-1, 1 ), center2-1, 2 );
+keyboard
 % plot
 if plotflag
   figure()
+  subplot(1,2,1)
   imagesc( x1, x2, pDistAve )
-  title('pair distribution g(x,y): density dependent')
+  title('pair distribution g(x,y): density dependent. normalized')
+  xlabel('x'); ylabel('y')
+  colorbar
+  subplot(1,2,2)
+  imagesc( x1, x2, pDistNoNorm )
+  title('pair distribution g(x,y): density dependent. no norm')
   xlabel('x'); ylabel('y')
   colorbar
 end
+
+keyboard
