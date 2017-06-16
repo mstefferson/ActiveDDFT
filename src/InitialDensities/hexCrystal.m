a016ab2f8f3de9894fa760992dea647bea06f01a
Michael Stefferson <mstefferson@gmail.com>
2017-06-16 15:01:40 -0600

    Finished hexCrystal maker. Removed testMap

--- src/InitialDensities/hexCrystal.m
+++ src/InitialDensities/hexCrystal.m
@@ -6,34 +6,32 @@
 % numPart: number of particles
 % aWant: desired crystal spacing
 % sigma: peak width
-
+%
 function crystal = hexCrystal(l, n, numPart, aWant, sigma)
-% build grid
-dx = l / n;
-x = 0:dx:l-dx;
-% mesh grid
-[x2mesh, x1mesh] = meshgrid( x, x );
 % get crystal lattice vectors
 % get spacing
 nPeaksCol = round(l/aWant);
 a = l / nPeaksCol;
 bWant = sqrt(3) * a / 2;
-nPeaksRow = round(l/bWant);
-b = l / nPeaksRow;
-colPlace = 0:nPeaksCol-1;
-rowPlace = 0:nPeaksRow-1;
-numPeaks = nPeaksRow * nPeaksCol;
+nPeaksRow = round(l/(2*bWant));
+nPeaksRow = 2 * nPeaksRow;
+b = l / (nPeaksRow);
+% build grid
+dx = l / n;
+x = 0:dx:l-dx;
+% mesh grid
+[x2mesh, x1mesh] = meshgrid( x, x );
+colPlace = 0:nPeaksCol;
+rowPlace = 0:nPeaksRow;
+numPeaks = (nPeaksCol+1) * (nPeaksRow+1);
 latticeInds = combvec( rowPlace, colPlace );
 % create lattice basis vectors
 a1 = [ 0; a ];
 a2 = [ b; a/2 ];
 transferMat = [ a2 a1]; % a2 first cause rowPlace in row 1
 crystalPeaks = transferMat * latticeInds;
-crystalPeaksWrap = mod( crystalPeaks, l );
+crystalPeaksWrap = mod( crystalPeaks, l+a );
 crystalPeaksWrap= crystalPeaksWrap.';
-% shift it off edges
-crystalPeaksWrap(:,1) = crystalPeaksWrap(:,1) + b/4;
-crystalPeaksWrap(:,2) = crystalPeaksWrap(:,2) + a/4;
 % loop over centers
 crystal = zeros( n, n );
 for ii = 1:numPeaks
