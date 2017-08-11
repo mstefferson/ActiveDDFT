8ad14a4ffac18c299501bbc6a23ddc6c2c109f94
Michael Stefferson <mstefferson@gmail.com>
2017-08-11 09:56:04 -0600

    cleaned up externPot. ready to merge

--- linearV.m
+++
@@ -1,6 +0,0 @@
-function [v, vFt, dv] = linearV( a, x )
-v = a * x;
-vFt = fftshift( fftn( v, a ) ) ;
-dv = a * ones( size(x) );
-end
-  
--- quadV.m
+++
@@ -1,6 +0,0 @@
-function [v, vFt, dv] = quadV( a, x )
-v = 1 / 2 * a * x .^ 2;
-vFt = fftshift( fftn( v, a ) ) ;
-dv = - a .* x;
-end
-  
