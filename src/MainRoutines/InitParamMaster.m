305c71ce5fbf95d51babf5fd7aaefde1c0e2f6db
Michael Stefferson <mstefferson@gmail.com>
2017-08-23 10:46:02 -0600

    edited comments in InitParamMaster

--- src/MainRoutines/InitParamMaster.m
+++ src/MainRoutines/InitParamMaster.m
@@ -34,7 +34,7 @@ particleMaster.interHb = 'mayer'; % Hard body interactions type mayer, spt
 % different cells are different runs
 particleMaster.interactLrV = {}; 
 % External potential (cell of cells): 
-%  { {'linV', dim, a}, {'quadV', dim, a}, {'nemV', es1} }
+%  { {'linV', dim, a}, {'quadV', dim, a}, {'nemV', es1, phase} }
 particleMaster.externalV = {}; % External potential 
 %%%%%%%%% System %%%%%%%%%%%%%%%%
 systemMaster.n1 = [64]; % Gridpoints in x dir (vec)
