30a707858804d0ed5282bd3a5cc1d9b596402091
Michael Stefferson <mstefferson@gmail.com>
2017-09-17 17:01:46 -0600

    tc: added plotOps and checkInitRho. Weird nematic behavior

---
+++ checkInitRho.m
@@ -0,0 +1,118 @@
+% Executeable for HardRod
+% Date
+function checkInitRho()
+dateTime =  datestr(now);
+fprintf('Starting checkRhoInit: %s\n', dateTime);
+% Add Subroutine path
+currentDir = pwd;
+addpath( genpath( [currentDir '/src'] ) );
+% Grab initial parameters
+if exist('Params.mat','file') == 0
+  if exist('initParams.m','file') == 0
+    cpParams();
+  end
+  fprintf('No Params.mat found. Running initParams\n');
+  initParams();
+end
+load Params.mat;
+fprintf('Params locked and loaded\n');
+movefile('Params.mat', 'ParamsRunning.mat');
+% Copy the master parameter list to ParamObj
+systemObj = systemMaster;
+particleObj = particleMaster;
+timeObj = timeMaster;
+rhoInit  = rhoInitMaster;
+flags    = flagMaster;
+runObj  = runMaster;
+diagOp = flags.DiagLop;
+trial = runObj.trialID;
+% Fix things
+% Change odd gridspacings to even unless it's one.
+if systemObj.n1 == 1
+  systemObj.l1 = 1;
+else
+  systemObj.n1 = systemObj.n1 + mod( systemObj.n1, 2 );
+end
+if systemObj.n2 == 1
+  systemObj.l2 = 1;
+else
+  systemObj.n2 = systemObj.n2 + mod( systemObj.n2, 2 );
+end
+if systemObj.n3 == 1
+  systemObj.l3 = 1;
+else
+  systemObj.n3 = systemObj.n3 + mod( systemObj.n3, 2 );
+end
+% Fix Ls if we want the box to be square
+if flags.SquareBox == 1
+  systemObj.L_box = unique( [systemObj.l1 systemObj.l2] );
+  systemObj.l1 = systemObj.L_box;
+  systemObj.l2 = systemObj.L_box;
+end
+% Fix l1 is we want all Ns to be the same
+if flags.AllNsSame == 1
+  if systemObj.n3 == 1
+    Nvec = unique( [systemObj.n1 systemObj.n2] );
+    systemObj.n1 = Nvec;  systemObj.n2 = Nvec;
+  else
+    Nvec = unique( [systemObj.n1 systemObj.n2 systemObj.n3] );
+    systemObj.n1 = Nvec;  systemObj.n2 = Nvec;   systemObj.n3 = Nvec;
+  end
+end
+% Make OP if making movies
+if flags.MakeMovies == 1; flags.MakeOP = 1; end % if make movie, make OP first
+%Currently, you must save
+if flags.MakeOP && flags.SaveMe == 0
+  fprintf('Turning on saving, you must be saving to make OPs (due to matfile)\n');
+  flags.SaveMe = 1;
+end
+if particleObj.vD  == 0; flags.Drive = 0; else flags.Drive = 1;end
+% fix rhoInit
+rhoInitObj = rhoInitManager( rhoInit, systemObj );
+% fix particles
+[particleObj, systemObj] = ...
+  particleInit( particleObj, systemObj, flags.DiagLop );
+systemObj.c = systemObj.bc ./ particleObj.b;
+systemObj.numPart  = systemObj.c * systemObj.l1 * systemObj.l2; % number of particles
+% Display everythin
+disp(runObj); disp(flags); disp(particleObj); disp(systemObj); disp(timeObj); disp(rhoInitObj);
+% rhoInit str
+initStr = rhoInitObj.fileStr;
+% Loops over all run
+fprintf('Starting loop over runs\n');
+% build grid
+[gridObj] = GridMakerPBCxk(systemObj.n1,systemObj.n2,systemObj.n3,...
+  systemObj.l1,systemObj.l2,systemObj.l3);
+% Make rho
+[rho] = MakeConc(systemObj,particleObj,rhoInitObj,gridObj);
+keyboard
+% Calc ops
+if systemObj.n3 > 1
+  % Commonly used trig functions
+  [~,~,phi3D] = meshgrid(gridObj.x2,gridObj.x1,gridObj.x3);
+  cosPhi3d = cos(phi3D);
+  sinPhi3d = sin(phi3D);
+  cos2Phi3d = cosPhi3d .^ 2;
+  sin2Phi3d = sinPhi3d .^ 2;
+  cossinPhi3d = cosPhi3d .* sinPhi3d;
+  [OPs.C,OPs.POP,OPs.POPx,OPs.POPy,OPs.NOP,OPs.NOPx,OPs.NOPy] = ...
+    OpCPNCalc(systemObj.n1,systemObj.n2,rho,...
+    gridObj.x3,cosPhi3d,sinPhi3d,cos2Phi3d,sin2Phi3d,cossinPhi3d);
+  cScale = particleObj.b;
+  plotOps( OPs, gridObj.x1, gridObj.x2, cScale )
+else
+  cScale = particleObj.b;
+  C = trapz_periodic( gridObj.x3, rho, 3 );
+  figure()
+  pcolor( gridObj.x1, gridObj.x2, cScale * C' )
+end
+
+dateTime =  datestr(now);
+movefile('ParamsRunning.mat', 'ParamsFinished.mat');
+runHr = floor( runTime / 3600); runTime = runTime - runHr*3600;
+runMin = floor( runTime / 60);  runTime = runTime - runMin*60;
+runSec = floor(runTime);
+fprintf('RunTime: %.2d:%.2d:%.2d (hr:min:sec)\n', runHr, runMin,runSec);
+fprintf('Finished RunHardRod: %s\n', dateTime);
+
+
---
+++ plotOps.m
@@ -0,0 +1,146 @@
+function plotOps( OPs, x, y, cScale )
+% set-up fonts
+fontSize = 30;
+% Find ticks
+nx = length(x);
+ny = length(y);
+lx = x(end) - 2*x(1) + x(2);
+ly = y(end) - 2*y(1) + y(2);
+xMid = x( nx/2 + 1);
+yMid = y( ny/2 + 1);
+xTick = [xMid-lx/4 xMid xMid+lx/4];
+yTick = [yMid-ly/4 yMid xMid+ly/4];
+xLim  = [x(1) x(end)];
+yLim  = [y(1) y(end)];
+if nargin < 4
+  cScale = 1;
+end
+% set-up subplot
+numRow = 1;
+numCol = 3;
+% Set up a index vector so quiver is too crowded
+DivNumX = 8;
+DivNumY = 8;
+DeltaX  = ceil(nx / DivNumX );
+DeltaY  = ceil(ny / DivNumY);
+% dir 1 = rows = x
+SubInd1 = 1:DeltaX:(nx + 1 - DeltaX);
+% dir 2 = columns = y
+SubInd2 = 1:DeltaY:(ny + 1 - DeltaX);
+% Set up figure, make it a square 0.8 of
+% smallest screen dimension
+ScreenSize = get(0,'screensize');
+ScreenWidth = ScreenSize(3); ScreenHeight = ScreenSize(4);
+FigWidth    = floor( ScreenWidth  );
+FigHeight   =  floor( ScreenHeight * .40);
+FigPos      = [ floor( 0.5 * ( ScreenWidth - FigWidth ) ) ...
+  floor( 0.5 * (ScreenHeight - FigHeight ) ) ...
+  FigWidth FigHeight];
+%Build a square box set by smallest dimension of screen
+Fig = figure();
+Fig.WindowStyle = 'normal';
+Fig.Position = FigPos;
+% Concentration
+axh1 = subplot(numRow,numCol,1); % Save the handle of the subplot
+axh1.TickLabelInterpreter = 'latex';
+h = colorbar('peer',axh1);
+h.TickLabelInterpreter = 'latex';
+axh1.NextPlot = 'replaceChildren';
+minC = min( OPs.C(:) );
+maxC = max( OPs.C(:) );
+if minC >= maxC -0.0001
+  maxC = 1.1 .* minC ;
+  minC = 0.9 .* minC;
+end
+axh1.CLim = cScale * [minC maxC];
+axh1.YTick = yTick;
+axh1.YLim = yLim;
+axh1.XTick = xTick;
+axh1.XLim = xLim;
+wantedTickLabel =  num2cell( yTick ) ;
+axh1.YTickLabel =  wantedTickLabel;
+shading(axh1,'interp');
+xlabel(axh1,'$$ x $$'); ylabel(axh1,'$$ y $$') 
+axh1.FontSize = fontSize;
+axis(axh1, 'square')
+cTitle = '$$ C $$';
+% Polar order
+axh2 = subplot(numRow,numCol,2); % Save the handle of the subplot
+axh2.TickLabelInterpreter = 'latex';
+h = colorbar('peer',axh2);
+h.TickLabelInterpreter = 'latex';
+axh2.NextPlot = 'replaceChildren';
+axh2.CLim = [0 1];
+axh2.XLim = xLim; %row and columns are flipped
+axh2.YLim = yLim; %row and columns are flipped
+axh2.XTick = xTick;
+axh2.YTick = yTick;
+axh2.YTickLabel =  wantedTickLabel;
+shading(axh2,'interp');
+xlabel(axh2,'$$ x $$'); ylabel(axh2,'$$ y $$') 
+axh2.FontSize = fontSize;
+axis(axh2, 'square')
+pTitle = '$$ P $$';
+% Nematic order
+axh3 = subplot(numRow,numCol,3); % Save the handle of the subplot
+axh3.TickLabelInterpreter = 'latex';
+h = colorbar('peer',axh3);
+h.TickLabelInterpreter = 'latex';
+axh3.NextPlot = 'replaceChildren';
+axh3.CLim = [0 1];
+axh3.XLim = xLim; %row and columns are flipped
+axh3.YLim = yLim; %row and columns are flipped
+axh3.XTick = xTick;
+axh3.YTick = yTick;
+axh3.YTickLabel = wantedTickLabel;
+shading(axh3,'interp');
+xlabel(axh3,'$$ x $$'); ylabel(axh3,'$$ y $$') 
+axh3.FontSize = fontSize;
+axis(axh3, 'square')
+nTitle = '$$ N $$';
+% Scale order parameters by it's max value to for it changes.
+cTemp =  cScale * OPs.C;
+polarTempX = OPs.POPx(SubInd1,SubInd2,:);
+polarTempY = OPs.POPy(SubInd1,SubInd2,:);
+maxPolar = max( max( max( OPs.POP(SubInd1,SubInd2,:) ) ) );
+polarTempX = polarTempX ./ maxPolar;
+polarTempY = polarTempY ./ maxPolar;
+nemTempX = OPs.NOPx(SubInd1,SubInd2,:);
+nemTempY = OPs.NOPy(SubInd1,SubInd2,:);
+maxNem = max( max( max( OPs.NOP(SubInd1,SubInd2,:) ) ) );
+nemTempX = nemTempX .* OPs.NOPx(SubInd1,SubInd2,:) ./ maxNem;
+nemTempY = nemTempY .* OPs.NOPy(SubInd1,SubInd2,:) ./ maxNem;
+% These matrices will need to be transposed to correct for x and y
+% loop over frames
+ii = 1;
+%% Concentration
+subplot(axh1);
+cla(axh1);
+pcolor( axh1, x, y, cTemp(:,:,ii)' );
+shading interp
+TitlStr = cTitle;
+title(axh1,TitlStr);
+pause(0.001);
+drawnow;
+%% Polar order
+subplot(axh2);
+cla(axh2);
+pcolor(axh2, x, y, OPs.POP(:,:,ii)' );
+shading interp
+TitlStr = pTitle;
+hold on
+quiver(axh2, x(SubInd1), y(SubInd2),...
+  polarTempX(:,:,ii)',...
+  polarTempY(:,:,ii)', 0,'color',[1,1,1] );
+title(axh2,TitlStr)
+%% Nematic order
+cla(axh3)
+subplot(axh3);
+pcolor(axh3, x, y, OPs.NOP(:,:,ii)');
+shading interp
+TitlStr = nTitle;
+hold on
+quiver(axh3, x(SubInd1),y(SubInd2),...
+  nemTempX(:,:,ii)', nemTempY(:,:,ii)',0,...
+  'color',[1,1,1],'ShowArrowHead','off','LineWidth',0.1);
+title(axh3,TitlStr)
