4a43c0813f29bc496fe9675e9707b0f09acd86d8
Michael Stefferson <mstefferson@gmail.com>
2017-09-29 09:12:39 -0600

    Added calc g1, g2 (op pair dist) flag

--- src/AnalysisFuncs/pairDistCalcRho.m
+++ src/AnalysisFuncs/pairDistCalcRho.m
@@ -3,12 +3,18 @@
 % Calculates the density dependent pair correlation function
 % g(r,r') = rho^(2) ./ rho^(1)rho^(1)
 %
-function [pDist] = pairDistCalcRho( rho, l1, l2, lRod, plotflag, saveName )
+function [pDist] = pairDistCalcRho( rho, l1, l2, lRod, calcOpDistFlag, ...
+  plotflag, saveName )
 % set saveMe
 if nargin == 4
+  calcOpDistFlag = 0;
   plotflag = 0;
   saveMe = 0;
 elseif nargin == 5
+  plotflag = 0;
+  saveMe = 0;
+elseif nargin == 6
+  plotflag = 0;
   saveMe = 0;
 elseif isempty( saveName )
   saveMe = 0;
@@ -48,7 +54,7 @@ trackProgMod =  ceil( n1 / 100);
 % calculate the pair dist
 for ii = 1:n1
   % set x
-  r1Temp = mayerInds1(ii); % Actual location of ii 
+  r1Temp = mayerInds1(ii); % Actual location of ii
   % set x+x'
   shiftInds1 = mod( (r1Temp-1) + allInds1 - 1, n1 ) + 1;
   for jj = 1:n2
@@ -65,21 +71,28 @@ for ii = 1:n1
       allInds3mm = allInds3(mm);
       for nn = 1:n3
         shiftInds3nn = shiftInds3(nn);
+        %g0
         mat2Intdelta0 =  expV( ii, jj, allInds3(nn), shiftInds3(mm) ) .* ...
           rho( shiftInds1, shiftInds2, shiftInds3nn ) .* rho(allInds1, allInds2, allInds3mm );
-        mat2Intdelta1 =  cosPhiTemp * mat2Intdelta0;
-        mat2Intdelta2 =  cosSqrPhiTemp * mat2Intdelta0;
         intOverRprime0(nn,mm) = trapz_periodic( x1, trapz_periodic( x2, mat2Intdelta0, 2 ), 1);
-        intOverRprime1(nn,mm)= trapz_periodic( x1, trapz_periodic( x2, mat2Intdelta1, 2 ), 1);
-        intOverRprime2(nn,mm) = trapz_periodic( x1, trapz_periodic( x2, mat2Intdelta2, 2 ), 1);
+        if calcOpDistFlag
+          %g1
+          mat2Intdelta1 =  cosPhiTemp * mat2Intdelta0;
+          intOverRprime1(nn,mm)= trapz_periodic( x1, trapz_periodic( x2, mat2Intdelta1, 2 ), 1);
+          %g2
+          mat2Intdelta2 =  cosSqrPhiTemp * mat2Intdelta0;
+          intOverRprime2(nn,mm) = trapz_periodic( x1, trapz_periodic( x2, mat2Intdelta2, 2 ), 1);
+        end
       end
     end
     delta0( ii, jj ) = trapz_periodic( phi, ...
       trapz_periodic( phi, intOverRprime0, 1 ), 2 );
-    delta1( ii, jj ) = trapz_periodic( phi, ...
-      trapz_periodic( phi, intOverRprime1, 1 ), 2 );
-    delta2( ii, jj ) = trapz_periodic( phi, ...
-      trapz_periodic( phi, intOverRprime2, 1 ), 2 );
+    if calcOpDistFlag
+      delta1( ii, jj ) = trapz_periodic( phi, ...
+        trapz_periodic( phi, intOverRprime1, 1 ), 2 );
+      delta2( ii, jj ) = trapz_periodic( phi, ...
+        trapz_periodic( phi, intOverRprime2, 1 ), 2 );
+    end
   end
   if mod( ii, trackProgMod  ) == 0
     fprintf('%f percent done\n', 100*ii/n1)
@@ -93,29 +106,35 @@ V = l1 .* l2;
 normFac = (nParticles^2 ) ./ V;
 % calculate pair distributions
 pDist0 = 1 / normFac * delta0; % pair dist
-pDist1 = delta1 ./ delta0; % polar dist
-pDist1(1,1) = 0; % get rid of dividing by zero
-pDist2 = (2*delta2-delta0) ./ delta0; % nem dist
-pDist2(1,1) = 0; % get rid of dividing by zero
-% Rotate it 
-pDist0Rot = rot90(pDist0);
-pDist1Rot = rot90(pDist1);
-pDist2Rot = rot90(pDist2);
-% center it. rows and columns are flopped from rotating 
-shiftColumn = round( n1 / 2 ); 
+if calcOpDistFlag
+  pDist1 = delta1 ./ delta0; % polar dist
+  pDist1(1,1) = 0; % get rid of dividing by zero
+  pDist2 = (2*delta2-delta0) ./ delta0; % nem dist
+  pDist2(1,1) = 0; % get rid of dividing by zero
+end
+% Rotate and center it
+shiftColumn = round( n1 / 2 );
 shiftRow = round( n2 / 2 );
+pDist0Rot = rot90(pDist0);
 pDist0RotCenter = circshift( circshift( pDist0Rot, -shiftRow+1, 1 ), shiftColumn, 2 );
-pDist1RotCenter = circshift( circshift( pDist1Rot, -shiftRow+1, 1 ), shiftColumn, 2 );
-pDist2RotCenter = circshift( circshift( pDist2Rot, -shiftRow+1, 1 ), shiftColumn, 2 );
+% center it. rows and columns are flopped from rotating
+if calcOpDistFlag
+  pDist1Rot = rot90(pDist1);
+  pDist2Rot = rot90(pDist2);
+  pDist1RotCenter = circshift( circshift( pDist1Rot, -shiftRow+1, 1 ), shiftColumn, 2 );
+  pDist2RotCenter = circshift( circshift( pDist2Rot, -shiftRow+1, 1 ), shiftColumn, 2 );
+end
 % store it
 pDist.pDist0 = pDist0;
-pDist.pDist1 = pDist1;
-pDist.pDist2 = pDist2;
 pDist.pDist0RotCenter = pDist0RotCenter;
-pDist.pDist1RotCenter = pDist1RotCenter;
-pDist.pDist2RotCenter = pDist2RotCenter;
+if calcOpDistFlag
+  pDist.pDist1 = pDist1;
+  pDist.pDist2 = pDist2;
+  pDist.pDist1RotCenter = pDist1RotCenter;
+  pDist.pDist2RotCenter = pDist2RotCenter;
+end
 % Save it
-if saveMe 
+if saveMe
   save(saveName,'pDist')
 end
 
@@ -125,5 +144,7 @@ if plotflag
   ttlstr = '$$g_0(x,y)$$';
   pairDistPlotSingle( pDist0RotCenter, l2, l1, ttlstr);
   % plot the three order parameter distribution functions
-  pairDistPlotOps( pDist0RotCenter, pDist1RotCenter, pDist2RotCenter, l2, l1)
+  if calcOpDistFlag
+    pairDistPlotOps( pDist0RotCenter, pDist1RotCenter, pDist2RotCenter, l2, l1)
+  end
 end
