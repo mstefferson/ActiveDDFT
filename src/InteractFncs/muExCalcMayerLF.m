9218ef3904e84941350c6f271ac1f6274d9659da
Michael Stefferson <mstefferson@gmail.com>
2017-10-25 11:16:16 -0600

    fixed tmp issue

--- src/InteractFncs/muExCalcMayerLF.m
+++ src/InteractFncs/muExCalcMayerLF.m
@@ -10,7 +10,7 @@
 function [muExFT] = muExCalcMayerLF(rhoFt,fmFt,systemObj,scaleFact, inds, indsMinus)
 % Calc scale factor if need be
 if nargin == 3
-  scaleFact = (systemObj.l3 * systemObj.l1 * systemObj.l2) ...
+  scaleFact = (systemObj.tmp * systemObj.l3 * systemObj.l1 * systemObj.l2) ...
     / (systemObj.n1 * systemObj.n2 * systemObj.n3^2);
   inds = 1:systemObj.n3;
   indsMinus = [1 systemObj.n3:-1:2];
